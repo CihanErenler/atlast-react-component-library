4e7eb1cadb56e224e565d3cea704c002e602048d
CihanErenler <cihanerenler@outlook.com>
2025-08-27 22:49:43 +0300

    add first version of button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,26 +1,51 @@
 import styles from "./styles.module.scss";
 import clsx from "clsx";
 
+const BUTTON_VARIANTS = {
+  primaty: "primary",
+  secondary: "secondary",
+  tertiary: "tertiary",
+  ghost: "ghost",
+  outline: "outline",
+  destructive: "destructive"
+}
+
 interface ComponentProps extends React.ComponentProps<"button"> {
-  primary?: boolean;
-  size?: "small" | "medium" | "large";
-  label: string;
+  // core props
+  disabled?: boolean;
+  type?: "button" | "submit" | "reset";
+  formId?: string;
+  loading?: boolean;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  title: string;
+// variants
+    variant?: "primary" | "secondary" | "tertiary" | "ghost" | "outline" | "destructive";
+  borderRadius?: "none" | "small" | "medium" | "full";
+  size?: "xs" | "sm" | "md" | "lg" | "xl"
+  fullWidth?: boolean;
 }
 
 export function Button({
-  primary = false,
-  size = "medium",
-  label,
+  variant = "primary",
+  size = "md",
+  title,
+  onClick
   ...props
 }: ComponentProps) {
+  const classNames = ["button"];
+
+  if(variant) {
+    BUTTON_VARIANTS.includes(variant) ? classNames.push(`button--${variant}`) : "pri";
+  }
+
   const style = clsx(styles.button, {
-    [styles["button--primary"]]: primary,
+    [styles[`button--${variant}`]]: variant,
     [styles[`button--${size}`]]: size,
   });
 
   return (
-    <button type="button" className={style} {...props}>
-      {label}
+    <button type="button" className={style} {...props} onClick={(e) => onclick}>
+      {title}
     </button>
   );
 }
